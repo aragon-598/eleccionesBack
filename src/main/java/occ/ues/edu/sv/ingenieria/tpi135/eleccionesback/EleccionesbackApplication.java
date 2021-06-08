793dd15a180e7bd969ed8022742858bd22fd4d5a
Alejandro Aragón <aragondru155@gmail.com>
2021-06-08 13:23:57 -0600

    Initial commit

---
+++ src/main/java/occ/ues/edu/sv/ingenieria/tpi135/eleccionesback/EleccionesbackApplication.java
@@ -0,0 +1,13 @@
+package occ.ues.edu.sv.ingenieria.tpi135.eleccionesback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleccionesbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EleccionesbackApplication.class, args);
+	}
+
+}
