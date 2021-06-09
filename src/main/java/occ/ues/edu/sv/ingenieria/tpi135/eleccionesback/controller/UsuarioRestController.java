04b02fff36a0f0afae5b6b5e40bc5303f9d85694
avalos <christian@avalosPC.com>
2021-06-08 23:24:12 -0600

    agregado el metodo para verificar logueo

--- src/main/java/occ/ues/edu/sv/ingenieria/tpi135/eleccionesback/controller/UsuarioRestController.java
+++ src/main/java/occ/ues/edu/sv/ingenieria/tpi135/eleccionesback/controller/UsuarioRestController.java
@@ -9,12 +9,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import occ.ues.edu.sv.ingenieria.tpi135.eleccionesback.entity.Usuarios;
 import occ.ues.edu.sv.ingenieria.tpi135.eleccionesback.repository.RepositorioUsuarios;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -48,5 +51,27 @@ public class UsuarioRestController {
             return new ResponseEntity<>(repoUsuarios.findAll(), HttpStatus.OK);
         }
 
+    }
+    
+    @PostMapping("/login")
+    public ResponseEntity<Object> verificarLogeo(@RequestBody Usuarios usr){
+        List<Usuarios> match = new ArrayList<>();
+        if (usr == null || repoUsuarios == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }else{
+            try {
+                 match= repoUsuarios.findAll().stream().filter(u -> u.getDui() == usr.getDui()).collect(Collectors.toList());
+            } catch (Exception e) {
+                 Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
+            }
+           
+            if (match.isEmpty()) {
+                return new ResponseEntity<>("El dui ingresado no existe", HttpStatus.NOT_FOUND);
+            }else if (!match.isEmpty() && !match.get(0).getContrasenia().equals(usr.getContrasenia())) {
+                return new ResponseEntity<>("La contraseña es invalida", HttpStatus.NOT_FOUND);
+            }else{
+                return new ResponseEntity<>(match.get(0), HttpStatus.ACCEPTED);
+            }
+        }
     }
 }
