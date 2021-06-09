f97e410fdef33f94c0831d1f743333ca49ee3056
avalos <christian@avalosPC.com>
2021-06-08 22:20:00 -0600

    modificacion en metodo get de usuarios

--- src/main/java/occ/ues/edu/sv/ingenieria/tpi135/eleccionesback/controller/UsuarioRestController.java
+++ src/main/java/occ/ues/edu/sv/ingenieria/tpi135/eleccionesback/controller/UsuarioRestController.java
@@ -5,10 +5,48 @@
  */
 package occ.ues.edu.sv.ingenieria.tpi135.eleccionesback.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import occ.ues.edu.sv.ingenieria.tpi135.eleccionesback.entity.Usuarios;
+import occ.ues.edu.sv.ingenieria.tpi135.eleccionesback.repository.RepositorioUsuarios;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author christian
  */
+@RestController
+@RequestMapping("usuario")
 public class UsuarioRestController {
-    
+
+    @Autowired
+    private RepositorioUsuarios repoUsuarios;
+
+    @GetMapping("/findAll")
+    public ResponseEntity<List<Usuarios>> findAll() {
+        List<Usuarios> lista = new ArrayList<>();
+
+        if (repoUsuarios == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } else {
+            try {
+                lista = repoUsuarios.findAll();
+            } catch (Exception e) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
+            }
+        }
+        if (lista.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } else {
+            return new ResponseEntity<>(repoUsuarios.findAll(), HttpStatus.OK);
+        }
+
+    }
 }
